fe327ada56c27f88c2f438b688becdef31186bfe
vigneshcj001 <vigneshwarancj@gmail.com>
2025-07-22 15:28:04 +0530

    Testing GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello from server!!");
 });
-
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+  });
+});
+app.post("/user", (req, res) => {
+  res.send("User created successfully!");
+});
+app.delete("/user",(req,res)=>{
+    res.send("User deleted successfully");
+});
 app.get("/test", (req, res) => {
   res.send("Hello from test route!!");
 });
